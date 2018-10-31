60e6210eeb7914e5e1ea753e2181f88e74b3ba40
YoRHazwz <1245469389@qq.com>
2018-11-01 00:15:25 +0800

    auto

--- Program_C/test.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-	printf("hello c!\n");
-	return 0;
-}
--- Program_C/test/test.c
+++ Program_C/test/test.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(void)
 {
-	printf("hello vim!\n");
+	printf("hello sublime!\n"); //中文
 	return 0;
 }
